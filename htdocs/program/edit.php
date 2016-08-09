6a99fcb5193bd4d790ec98e25f39d90f6435a6a4
Johan Van de Wauw <Johan.VandeWauw@realdolmen.com>
2016-08-09 11:53:41 +0200

    Fix sample

--- htdocs/program/edit.php
+++ htdocs/program/edit.php
@@ -6,7 +6,10 @@ $db = 'foss4g-2016';
 mysql_connect(localhost,$user,$password);
 @mysql_select_db($db) or die( "Unable to select database");
 $query='select * FROM presentations';
-mysql_query($query);
+$result = mysql_query($query);
+if (!$result) {
+    die('Could not query:' . mysql_error());
+    }
 echo mysql_result($result, 0, "presenter");
 mysql_close();
 
