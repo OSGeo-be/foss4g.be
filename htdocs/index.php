e4cc3d11e6b00533e805267aa6681759da067ab7
Johan Van de Wauw <johan@vandewauw.be>
2016-08-23 21:50:19 +0200

    Add programme to index and menu

--- htdocs/index.php
+++ htdocs/index.php
@@ -41,6 +41,7 @@
                 <img src="<?php echo $lang['CECI']; ?>" alt="<?php echo $lang['SITE_NAME']; ?> - <?php echo $lang['CECI_1']; ?> <?php echo $lang['CECI_2']; ?> <?php echo $lang['CECI_3']; ?>">
                 <ul class="actions">
                     <li><a href="inscription.php" class="button"><?php echo $lang['MENU_REG']; ?></a></li>
+                    <li><a href="programme.php" class="button"><?php echo $lang['PROGRAMME']; ?></a></li>
 					<li><a href="maps.php" class="button"><?php echo $lang['MENU_MAPS']; ?></a></li>
                     <li><a href="call.php" class="button"><?php echo $lang['MENU_CALL2']; ?></a></li>
                 </ul>
