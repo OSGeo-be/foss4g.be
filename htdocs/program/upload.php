401d07cc267f38ba6b29fcc2d2046e763e24f56e
Johan Van de Wauw <Johan.VandeWauw@realdolmen.com>
2016-09-27 16:39:54 +0200

    Also allow pdf

--- htdocs/program/upload.php
+++ htdocs/program/upload.php
@@ -26,8 +26,8 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+&& $imageFileType != "gif" && $imageFileType != "pdf" ) {
+    echo "Sorry, only JPG, JPEG, PNG, PDF & GIF files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
