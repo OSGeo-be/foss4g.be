0c1522b5912c2d9a40c43703de1500b6a86276c2
Johan Van de Wauw <johan@gisky.be>
2017-09-21 21:07:56 +0200

    Add sponsors

--- htdocs/sponsors.php
+++ htdocs/sponsors.php
@@ -52,8 +52,9 @@
 						  </dd>						  
 						  <dt class="spons"><a href="#" class="accordionTitle sponsors gold"> <?php echo $lang['SPONSORS_2_TITLE']; ?> <span class="spon2"><?php echo $lang['SPONSORS_2_TAGLINE']; ?></span></a></dt>
 						  <dd class="accordionItem accordionItem">
+						    <a href="https://overheid.vlaanderen.be/informatie-vlaanderen"  class="sponsor icon aiv"><span class="label">Agentschap Informatie Vlaanderen</span></a>
 							<a href="http://www.champs-libres.coop/" class="sponsor icon champs-libres"><span class="label">Champs-libres</span></a>
-                            				<a href="http://www.gim.be/" class="sponsor icon gim"><span class="label">GIM</span></a>
+                            <a href="http://www.gim.be/" class="sponsor icon gim"><span class="label">GIM</span></a>
 						  </dd>
 						  <dt class="spons"><a href="#" class="accordionTitle sponsors silver"> <?php echo $lang['SPONSORS_3_TITLE']; ?> <span class="spon3"><?php echo $lang['SPONSORS_3_TAGLINE']; ?></span></a></dt>
 						  <dd class="accordionItem accordionItem">
