3a57dccc05596a1dc634a0959037591c3d7765b3
Johan Van de Wauw <johan@gisky.be>
2017-10-03 13:43:28 +0200

    Fix mysql finally :-)

--- htdocs/mysql.php
+++ htdocs/mysql.php
@@ -7,5 +7,6 @@ $link = mysqli_connect('localhost',$user,$password,$db);
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
         exit();
-	};
+};
+mysqli_set_charset($link, 'utf8mb4');
 ?>
