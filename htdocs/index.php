a2401d91cbbd3234d834b79a9e8de67e1489ab53
Johan Van de Wauw <johan@gisky.be>
2018-05-06 22:34:24 +0200

    Add call for papers/remove schedule/.. from front page

--- htdocs/index.php
+++ htdocs/index.php
@@ -34,9 +34,7 @@
                 <img src="<?php echo $lang['CECI']; ?>" alt="<?php echo $lang['SITE_NAME']; ?> - <?php echo $lang['CECI_1']; ?> <?php echo $lang['CECI_2']; ?> <?php echo $lang['CECI_3']; ?>">
                 <ul class="actions">
 				<!-- Button  -->
-				<li><a href="inscription.php" class="button"><?php echo $lang['MENU_REG']; ?></a></li>
-				
-                    <li><a href="programme.php" class="button"><?php echo $lang['MENU_PROGRAM']; ?></a></li>
+                    <li><a href="papers.php" class="button"><?php echo $lang['MENU_CALL']; ?></a></li>
 					<li><a href="maps.php" class="button"><?php echo $lang['MENU_MAPS']; ?></a></li>
                     <li><a href="call.php" class="button"><?php echo $lang['MENU_CALL2']; ?></a></li>
                 </ul>
