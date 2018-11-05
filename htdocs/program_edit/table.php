40b0ac6f0fa283a93c090742f787b375b0ed3985
Johan Van de Wauw <johan@gisky.be>
2018-11-05 18:08:22 +0100

    Add edit link for plenary track

--- htdocs/program_edit/table.php
+++ htdocs/program_edit/table.php
@@ -34,7 +34,7 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 {
 	if ($row['track']==0)
 	{
-		printf ("<tr><td>%s</td><td colspan='4'>%s</td></tr>", $row['start'], $row['title']);
+		printf ("<a href=edit.php?id=%s>X</a><tr><td>%s</td><td colspan='4'>%s</td></tr>", $row['start'], $row['title']);
 	}
 	else
 	{
