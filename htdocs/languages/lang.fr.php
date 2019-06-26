abf23d67b67c2e45fd14274f5df1bebcaa6debb5
Johan Van de Wauw <johanvdw@fosdem.org>
2019-06-26 18:29:28 +0000

    Fix dangling quoting errors

--- htdocs/languages/lang.fr.php
+++ htdocs/languages/lang.fr.php
@@ -277,7 +277,7 @@ $lang['INFO_TT_CAR'] ='Parking Tour & Taxis / 3 euros/heure';
 // PAGE INSCRIPTION ------------------------------------------------------------------------------------------------/
 
 $lang['REGISTRATION_TXT'] ='L’événement est <u>gratuit pour tous</u>, mais les places sont limitées et l\'inscription est donc obligatoire!';
-$lang['REGISTRATION_SURVEYORS_TXT'] = "La conférence FOSS4G BELGIUM '.$lang['YEAR'].' a également été reconnue par le Conseil fédéral des géomètres-experts et peut donner jusqu’à 5 heures de formation permanente, selon le temps de présence effective.<br/>
+$lang['REGISTRATION_SURVEYORS_TXT'] = 'La conférence FOSS4G BELGIUM '.$lang['YEAR']." a également été reconnue par le Conseil fédéral des géomètres-experts et peut donner jusqu’à 5 heures de formation permanente, selon le temps de présence effective.<br/>
 Bien que notre évènement soit gratuit pour tous, des frais de dossiers s'élevant à 25 € seront demandés pour l'obtention de cette attestation.<br/>
 Si vous souhaitez obtenir une attestation, merci de bien vouloir vous inscrire en sélectionnant un ticket \"Surveyor\". Les organisateurs reprendront par la suite contact avec vous afin de régler les formalités.";
 
