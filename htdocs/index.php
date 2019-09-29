fc8b5a1dc7f9cb7bb9551c0758138c79320e8c67
Johan Van de Wauw <johan@gisky.be>
2019-09-29 12:04:53 +0200

    Add Registration button to homepage instead of call for papers

--- htdocs/index.php
+++ htdocs/index.php
@@ -31,7 +31,7 @@
                 <!-- Button  -->
 <!--
                     <li><a href="programme.php" class="button"><?php echo $lang['MENU_PROGRAM']; ?></a></li>-->
-					<li><a href="papers.php" class="button"><?php echo $lang['MENU_CALL3']; ?></a></li>
+					<li><a href="inscription.php" class="button"><?php echo $lang['MENU_REG']; ?></a></li>
 					<li><a href="maps.php" class="button"><?php echo $lang['MENU_MAPS']; ?></a></li>
                     <li><a href="call.php" class="button"><?php echo $lang['MENU_CALL2']; ?></a></li>
                 </ul>
