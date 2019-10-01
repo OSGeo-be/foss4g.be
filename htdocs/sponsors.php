f3fddeb2e3c993c2c6d42dd2fbeb61c68a48091b
Johan Van de Wauw <johan@gisky.be>
2019-10-01 15:20:08 +0200

    Add anyways as a sponsor (thanks!)

--- htdocs/sponsors.php
+++ htdocs/sponsors.php
@@ -53,15 +53,16 @@
 						  <dt class="spons"><a href="#" class="accordionTitle sponsors gold"> <?php echo $lang['SPONSORS_2_TITLE']; ?> <span class="spon2"><?php echo $lang['SPONSORS_2_TAGLINE']; ?></span></a></dt>
 						  <dd class="accordionItem accordionItem">
 <?php /*<a href="https://overheid.vlaanderen.be/informatie-vlaanderen"  class="sponsor icon aiv"><span class="label">Agentschap Informatie Vlaanderen</span></a> */?>
-							<a href="http://www.champs-libres.coop/" class="sponsor icon champs-libres"><span class="label">Champs-libres</span></a>
-                            <a href="http://www.gim.be/" class="sponsor icon gim"><span class="label">GIM</span></a>
+							<a href="https://www.anyways.eu/" class="sponsor icon anyways"><span class="label">Anyways</span></a>
+							<a href="https://www.champs-libres.coop/" class="sponsor icon champs-libres"><span class="label">Champs-libres</span></a>
+                            <a href="https://www.gim.be/" class="sponsor icon gim"><span class="label">GIM</span></a>
 			    <a href="http://ngi.be/" class="sponsor icon ngi"><span class="label">NGI</span></a>
-                            <a href="http://oslandia.com/" class="sponsor icon oslandia"><span class="label">Oslandia</span></a><!--
+                            <a href="https://oslandia.com/" class="sponsor icon oslandia"><span class="label">Oslandia</span></a><!--
                             <a href="https://www.fabspace.eu/" class="sponsor icon fabspace"><span class="label">Fabspace 2.0</span></a>
                                 <a href="http://www.geosolutions.be" class="sponsor icon geosolutions"><span class="label">Geosolutions</span></a>
 -->
 			    				<a href="https://spw.wallonie.be/" class="sponsor icon spw"><span class="label">Spw</span></a>
-						<a href="http://www.idgeo.fr" class="sponsor icon idgeo"><span class="label">idgeo</span></a>
+						<a href="https://www.idgeo.fr" class="sponsor icon idgeo"><span class="label">idgeo</span></a>
 						  </dd>
 						  <dt class="spons"><a href="#" class="accordionTitle sponsors silver"> <?php echo $lang['SPONSORS_3_TITLE']; ?> <span class="spon3"><?php echo $lang['SPONSORS_3_TAGLINE']; ?></span></a></dt>
 						  <dd class="accordionItem accordionItem">
