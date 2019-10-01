56291ed84557eafe885767f948f1455e95755627
Johan Van de Wauw <johan@gisky.be>
2019-10-01 10:54:37 +0200

    Add programme to menu and frontpage

--- htdocs/index.php
+++ htdocs/index.php
@@ -29,11 +29,10 @@
                 <img src="<?php echo $lang['CECI']; ?>" alt="<?php echo $lang['SITE_NAME']; ?> - <?php echo $lang['CECI_1']; ?> <?php echo $lang['CECI_2']; ?> <?php echo $lang['CECI_3']; ?>">
                 <ul class="actions">
                 <!-- Button  -->
-<!--
-                    <li><a href="programme.php" class="button"><?php echo $lang['MENU_PROGRAM']; ?></a></li>-->
+                    <li><a href="programme.php" class="button"><?php echo $lang['MENU_PROGRAM']; ?></a></li>
 					<li><a href="inscription.php" class="button"><?php echo $lang['MENU_REG']; ?></a></li>
 					<li><a href="maps.php" class="button"><?php echo $lang['MENU_MAPS']; ?></a></li>
-                    <li><a href="call.php" class="button"><?php echo $lang['MENU_CALL2']; ?></a></li>
+<!--                    <li><a href="call.php" class="button"><?php echo $lang['MENU_CALL2']; ?></a></li>-->
                 </ul>
                 <p><?php echo $lang['SITE_DATABIG']; ?></p>
 			</section>
