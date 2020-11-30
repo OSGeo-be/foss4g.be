ef91e6947f39a810ce47c044b27803f779a35d84
Johan Van de Wauw <johan@gisky.be>
2020-11-30 12:09:15 +0100

    Add missing file to repo

---
+++ htdocs/program/abstract.php
@@ -0,0 +1,19 @@
+<?php
+include_once("../mysql.php");
+
+$start=$_GET['start'];
+$track=$_GET['track'];
+
+?>
+<?php
+$query = "select abstract FROM presentations where start=? and track=?";
+$stmt = mysqli_stmt_init($link);
+$stmt = mysqli_prepare($link, $query);
+mysqli_stmt_bind_param($stmt,"si",$start,$track);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_result($stmt,$abstract);
+mysqli_stmt_fetch($stmt);
+header('Content-type: application/json');
+echo json_encode(htmlentities($abstract));
+
+?>
