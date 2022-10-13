b289db485478ca28417219748e500ca73de4291f
Marc Ducobu <marc.ducobu@champs-libres.coop>
2022-10-13 20:30:39 +0200

    Re integrations of pages in normal website

---
+++ htdocs/call_all_lang.php
@@ -0,0 +1,256 @@
+<?php include_once 'common.php'; ?>
+<!DOCTYPE html>
+<html lang="<?php echo $lang['URL_SHORT']; ?>">
+	<head>
+		<title><?php echo $lang['SITE_TITLE']; ?></title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="<?php echo $lang['SITE_DESCRIPTION']; ?>" />
+		<meta name="keywords" content="<?php echo $lang['SITE_KEYWORDS']; ?>" />
+		<meta name="geo.placename" content="Avenue du Port 86 C/3002, Tour & Taxis, 1000 Brussels, Belgium" />
+		<meta name="geo.position" content="50.866248;4.349073" />
+		<meta name="geo.region" content="BE-Brussels" />
+		<meta name="ICBM" content="50.866248, 4.349073" />	
+		<link rel="canonical" href="http://www.foss4g.be/" />
+		<script src="/js/jquery.min.js" type="text/javascript"></script>
+		<script src="/js/jquery.dropotron.min.js" type="text/javascript"></script>
+		<script src="/js/jquery.scrollgress.min.js" type="text/javascript"></script>
+		<script src="/js/skel.min.js" type="text/javascript"></script>
+		<script src="/js/skel-layers.min.js" type="text/javascript"></script>
+		<script src="/js/init.js" type="text/javascript"></script>
+		<!--[if lte IE 8]><script src="/css/ie/html5shiv.js"></script><![endif]-->	
+		<noscript>
+			<link rel="stylesheet" href="/css/skel.css" />
+			<link rel="stylesheet" href="/css/style.css" />
+			<link rel="stylesheet" href="/css/style-wide.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="/css/ie/v8.css" /><![endif]-->
+
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<?php include 'header.inc'; ?>
+			</header>
+
+		<!-- Main -->
+			<section id="main" class="container">
+				<header>
+					<h2><?php echo $lang['SPONSORS_TITLE']; ?></h2>
+					<p><?php echo $lang['SPONSORS_SUBTITLE']; ?></p>											  
+				</header>
+			</section>
+
+<div class="row">
+    <div class="6u">
+      <section class="box plenary">
+        <h3>Français</h3>
+
+        <h4 style="text-align:left">A propos de FR FOSS4G / State of the map Belgium</h4>
+
+        <p>
+          Après les succès des précédentes éditions des F0SS4G Belgium, OSGeo.be organisera le jeudi 17 novembre 2022 une nouvelle édition de la conférence FOSS4G Belgium. Cette édition est co-organisée avec OpenStreetMap Belgium.
+        </p>
+        
+        <p>
+          Les conférences FOSS4G rassemblent les développeurs et les utilisateurs (pouvant être aussi bien des managers que des décideurs) de logiciels libres en géomatique du monde entier afin de parler des nouvelles directions, de passionnantes mises en œuvre et des opportunités commerciales grandissantes du domaine. Ces dernières années ont vu beaucoup de changements dans le secteur de la géomatique. Un de ces changements est la montée en maturité de ces solutions ainsi que leur déploiement. Dans beaucoup de cas, les organisations utilisent un mixte de solutions ouvertes et propriétaires.
+        </p>
+
+        <p> 
+          State of the Map Belgium cartographie l'état du projet OpenStreetMap en Belgique. Les contributeurs y expliquent leurs manières de cartographier afin d’améliorer la qualité des données. Les réutilisateurs y exposent leurs projets créatifs basés sur l’exploitation des données.
+        </p>
+
+        <p>
+          Nous sommes à la recherche de sponsors pour le sixième FOSS4G belge.
+        </p>
+      
+        <h4 style="text-align:left">Public ciblé</h4>
+
+        <p>Le but de la conférence est de toucher un public de 160 participants (pour cette édition).</p>
+        <p>La conférence comprendra des séances plénières d'orateurs de renom, une session principale présentant des cas d'utilisation, une session parallèle destinée aux informaticiens où sera présenté les particularités de l'utilisation de solutions open-source en géomatique au sein d'institutions et une dernière session destinée aux développeurs de ces solutions.</p>
+        <p>La session “State of the map” cible toute personne impliquée, ou voulant s’impliquer dans a communauté OSM d’une manière ou d’une autre (utilisateurs, contributeurs, …).</p>
+        <p>Les présentations et les débats se tiendront en anglais, ainsi qu'en néerlandais et en français.</p>
+        <p>Plus d'informations seront données sur le site de l'événement : http://2022.foss4g.be.</p>
+
+        <h4 style="text-align:left">Date et lieu</h4>
+
+        <p>L'événement se tiendra le jeudi 17 novembre 2022 dans la splendide salle de conférence de BOSA qui supporte l'événement en fournissant le lieu.</p>
+
+        <h4 style="text-align:left">Sponsorisation et avantages</h4>
+
+        <p>Voici la liste des différents niveaux de sponsorisation et les avantages qu'ils apportent :</p>
+		
+		<div class="accordion">
+			<dl>
+				<dt>
+					<a href="#" class="accordionTitle gold"> Gold <span class="spon2"><?php echo $lang['SPONSORS_2_TAGLINE']; ?></span></a>
+				</dt>
+				<dd class="accordionItem">
+					<a href="#" class="button gold icon fa-check">Stand de présentation</a>
+					<a href="#" class="button gold icon fa-check">1/2 page dans le programme</a>
+					<a href="#" class="button gold icon fa-check">Grand logo sur la page principale du site web</a>
+					<a href="#" class="button gold icon fa-check">Logo sur la page "sponsors" du site web</a>
+					<a href="#" class="button gold icon fa-check">Liste des participants</A>
+				</dd>
+				<dt>
+					<a href="#" class="accordionTitle bronze"> Bronze <span class="spon4"><?php echo $lang['SPONSORS_4_TAGLINE']; ?></span></a>
+				</dt>
+				<dd class="accordionItem">
+					<a href="#" class="button gold icon fa-check">Petit logo sur la page principale du site web</a>
+					<a href="#" class="button gold icon fa-check">Logo sur la page "sponsors" du site web</a>
+					<a href="#" class="button gold icon fa-check">Liste des participants</A>
+				</dd>
+			</dl>
+		</div>
+
+        <h4 style="text-align:left">Contact</h4>
+
+        <p>Si vous êtes intéressé pour sponsoriser l'événement FOSS4G-BE, veuillez contacter <a href="mailto:sponsors@foss4g.be">sponsors@foss4g.be</a>.</p>
+
+        <h4 style="text-align:left">OSGeo</h4>
+        
+        <p>La fondation Open Source Geospatial (OSGeo), est une organisation sans but lucratif dont la mission est d'aider et de promouvoir le développement collaboratif des données et des technologies ouvertes en géomatique. La fondation fournit une aide financière, organisationnelle et légale à l'immense communauté open-source de ce domaine. En outre, elle sert d'entité légale indépendante à laquelle les membres de la communauté peuvent contribuer (développements informatiques, dons, autres ressources) tout en ayant l'assurance que leurs contributions resteront au bénéfice de tous. Aussi, l'OSGeo sert d'assistance et de défense pour la communauté géospatiale libre, et fournit un forum commun et une infrastructure partagée pour améliorer la collaboration entre ses différents projets.</p>
+
+        <p>Les projets de la fondation sont tous librement disponibles et utilisables sous une licence open source certifiée par l'OSI.</p>
+
+        <ul>
+          <li>
+            <b>Votre organisation dépend d'un logiciel de l'OSGeo.</b>
+            Votre entreprise ou votre institution utilise des technologies de l'OSGeo dans son travail quotidien, ce qui lui permet d'être compétitive et de fournir des services d'une manière rentable. En soutenant l'OSGeo, vous soutenez votre propre activité à long terme sachant qu'une OSGeo solide soutient vos projets.
+          </li>
+          <li>
+            <b>Vous représentez une communauté qui a adopté les logiciels de l'OSGeo.</b>
+            Des communautés représentant un large éventail de domaines, allant des associations de professionnels aux organisations non-gouvernementales, tirent profit des logiciels de l'OSGeo qui sont un composant critique de leurs produits finaux. Il y a un intérêt à ce que votre communauté s'assure de la pérennité du logiciel utilisé. En sponsorisant l'OSGeo, vous montrez votre soutien au logiciel choisi par vos membres.
+          </li>
+          <li>
+            <b>Vos objectifs sont similaires à ceux de l'OSGeo concernant le soutien aux standards ouverts ou à d'autres priorités.</b>
+            Aidez votre propre organisation à atteindre ses objectifs en sponsorisant l'OSGeo en tant qu'organisation proche de la votre et bénéficiez des actions volontaristes d'une communauté mondiale prenant rapidement de l'ampleur.
+          </li>
+          <li>
+            <b>Montrez votre leadership dans les technologies open source en géomatique.</b>
+            En sponsorisant l'OSGeo vous allez associer votre entreprise aux activités de milliers d'utilisateurs de logiciels de l'OSGeo à travers le monde. En tant que sponsor de l'OSGeo, vous allez lier la croissance de votre entreprise à l'élan impulsé par l'OSGeo dans le monde professionnel.
+          </li>
+          <li>
+            <b>Profitez des opportunités de mise en réseau</b>
+            En tant que sponsor de l'OSGeo, vous allez bénéficier d'opportunités uniques de rencontrer et partager vos idées avec des membres clés de l'OSGeo, avec d'autres sponsors de l'OSGeo et avec les personnes et les organisations affiliées à l'OSGeo.
+          </li>
+        </ul>
+
+        <h4 style="text-align:left">OSGeo-BE</h4>
+
+        <p>Le naissant chapitre belge de l'OSGeo a pour but de transposer les missions générales de l'OSGeo au contexte belge en organisant des événements de réseautage et d'information.</p>
+
+        <h4 style="text-align:left">OpenStreetMap Belgium</h4>
+
+        <p>OpenStreetMap Belgium est un chapitre local et officiel de la Fondation OpenStreetMap et est membre d’Open Knowledge Belgium. Notre organisation soutient la croissance de la communauté et du projet en Belgique. Nous mettons en place des projets pour améliorer la qualité des données. De plus, proposons de mettre en lien la communauté et nos Corporate Members via l’adresse email <a href="mailto:community@osm.be">community@osm.be</a>.</p>
+
+      </section>
+    </div>
+
+    <div class="6u">
+      <section class="box plenary">
+        <h3>Nederlands</h3>
+
+        <h4 style="text-align:left">Over FOSS4G / State of the map Belgium 2022</h4>
+
+        <p>Na een korte pauze hervatten we het succesverhaal van FOSS4G Belgium. OSGeo.be en OpenStreetMap Belgium organiseren op dondardag 17 november 2022 een nieuwe editie.</p>
+        <p>De FOSS4G conferenties brengen ontwikkelaars en gebruikers van open source geo tools samen, om er te praten over nieuwe richtingen waarin de ontwikkeling gaat, mooie toepassingen en de toenemende commerciële kansen in de sector. De laatste jaren zijn er veel veranderingen in de geo sector. Het toenemende gebruik en de volwassenheid van open tools zijn daar een typisch voorbeeld van. Vaak worden open en eigendomsmatige producten gecombineerd.</p>
+        <p>State of the Map Belgium brengt in kaart hoe het staat met het OpenStreetMap project in België. Bijdragers aan het project laten zien hoe ze ervoor zorgen dat hun kaartdata de kwaliteit bereikt die steeds meer projecten doet overstappen. Hergebruikers tonen de creatieve manieren waarop ze met de data aan de slag gaan.</p>
+        <p>We zijn op zoek naar sponsors voor deze zesde editie van FOSS4G Belgium.</p>
+        
+        <h4 style="text-align:left">Doelpubliek</h4>
+        <p>We mikken met deze editie op een totaal publiek van 160 deelnemers.</p>
+        <p>De conferentie omvat plenaire sessies met bekende sprekers, een hoofdpresentatie met een gebruikscase, een parallelle sessie die meer op informatici is gericht die open-source gebruiken binnen organisaties en een derde sessie die meer gericht is op software ontwikkelaars.</p>
+        <p>Het luik State of the Map mikt op huidige en toekomstige gebruikers van OpenStreetMap en op iedereen die op een of andere manier lid is van de community.</p>
+        <p>De presentaties en debatten vinden plaats in het Engels, alsook in het Frans en Nederlands.</p>
+        <p>Meer informatie Plus d'informations seront données sur le site de l'événement : http://2022.foss4g.be.</p>
+        
+        <h4 style="text-align:left">Plaats & datum</h4>
+        <p>Het evenement gaat door op donderdag 17 november 2022 in de mooie conferentiezaal van
+        BOSA , die de conferentie ondersteunen door deze zaal aan te bieden.</p>
+
+        <h4 style="text-align:left">Sponsering & voordelen</h4>
+
+		<div class="accordion">
+			<dl>
+				<dt>
+					<a href="#" class="accordionTitle gold"> Gold <span class="spon2"><?php echo $lang['SPONSORS_2_TAGLINE']; ?></span></a>
+				</dt>
+				<dd class="accordionItem">
+					<a href="#" class="button gold icon fa-check">Stand</a>
+					<a href="#" class="button gold icon fa-check">1/2 pagina in het programma-boekje</a>
+					<a href="#" class="button gold icon fa-check">Groot logo op de landings-pagina van de website</a>
+					<a href="#" class="button gold icon fa-check">Logo op de sponsors pagina van de website</a>
+					<a href="#" class="button gold icon fa-check">Deelnemers-lijst</a>
+				</dd>
+				<dt>
+					<a href="#" class="accordionTitle bronze"> Bronze <span class="spon4"><?php echo $lang['SPONSORS_4_TAGLINE']; ?></span></a>
+				</dt>
+				<dd class="accordionItem">
+					<a href="#" class="button gold icon fa-check">Klein logo op de landings-pagina van de website</a>
+					<a href="#" class="button gold icon fa-check">Logo op de sponsors pagina van de website</a>
+					<a href="#" class="button gold icon fa-check">Deelnemers-lijst</a>
+				</dd>
+			</dl>
+		</div>        
+        
+        <h4 style="text-align:left">Contact</h4>
+        
+        <p>Als u interesse heeft om het event te ondersteunen, neem dan contact op met <a href="mailto:sponsors@foss4g.be">sponsors@foss4g.be</a>.</p>
+         
+        <h4 style="text-align:left">OSGeo</h4>
+        <p>De Open Source Geospatial Foundation (OSGeo), is een non-profit organisatie met als missie het ondersteunen van de open samenwerking aan open source geospatial software, en het gebruik van die software aan te moedigen. Deze Foundation geeft financiële, organisationele en juridische steun aan de bredere open source geospatial community. Het is ook een rechtspersoon die gebruikt kan worden als eigenaar van code, financiële middelen en andere bezittingen, wetende dat deze producten steeds zullen bijdragen aan het algemeen belang. OSGeo doet ook dienst als een belangenorganisatie voor de open source geospatial community, en voorziet een gemeenschappelijk forum en gedeelde infrastructuur om samenwerking over meerdere projecten te bevorderen.</p>
+        <p>Alle projecten van de Foundation zijn vrij beschikbaar onder een open source licentie gecertifieerd door de OSI.</p>
+        <p>Extra voordelen</p>
+        <ul>
+          <li>
+            <b>Uw bedrijf is afhankelijk van OSGeo software.</b>
+            Uw organisatie gebruik OSGeo technologie in zijn dagelijks werk, wat toelaat om competitief te zijn en een scherp geprijsd product aan te bieden. Door OSGeo te ondersteunen, draagt u bij aan de lange termijn duurzaamheid van uw eigen organisatie.
+          </li><li>
+            <b>Uw organisatie is afhankelijk van OSGeo software.</b>
+            Allerlei organisaties en domeinen maken gebruik van OSGeo technologie, van verenigingen van professionals tot NGO’s. Door OSGeo te ondersteunen, draagt u bij aan de lange termijn duurzaamheid van de producten die uw leden verkiezen. 
+          </li><li>
+            <b>Uw doelstellingen zijn gelijkaardig aan die van OSGeo wat betreft open standaarden en andere prioriteiten.</b>
+            Help uw eigen organisatie zijn doelstelling te bereiken door je medestanders de versterken.
+          </li><li>
+            <b>Toon je leiderschap in open source geo technologie.</b>
+            Door OSGeo te sponseren, associeert u uw eigen onderneming met de activiteiten van duizenden gebruikers van de de tools van OSGeo. U surft dan mee op de groei van OSGeo in de professionele wereld.
+          </li><li>
+            <b>Profiteer van de kansen in het netwerk.</b>
+            Als sponsor van OSGeo, heeft u een unieke kans om uit te wisselen met de kern van OSGeo, met andere sponsors en met mensen die lid zijn van de aan OSGeo geaffilieerde organisaties.
+        </ul>
+
+        <h4 style="text-align:left">OSGeo-BE</h4>
+        
+        <p>De Belgische afdeling vertaalt de algemene doelstellingen van OSGeo naar de Belgische context, vooral door het organiseren van evenementen ter verspreiding van informatie.</p>
+
+        <h4 style="text-align:left">OpenStreetMap Belgium</h4>
+
+        <p>We zijn een officieel Local Chapter van de OpenStreetMap Foundation en een Activity van Open Knowledge Belgium. Onze organisatie ondersteunt de groei van de community en het project in België. We zetten projecten op om de kaartdata verder te verbeteren. Daarnaast zijn we uw interface met de community, via ons support adres <a href="mailto:community@osm.be">community@osm.be</a> en voor onze Corporate Members.</p>
+ 
+      </section>
+    </div>
+</div>
+
+
+
+
+
+			
+		<!-- CTA -->
+			<section id="cta">
+				
+				<p><?php echo $lang['FOOTER_THANKS']; ?><p>
+				<p><?php echo $lang['FOOTER_THANKS_ALL']; ?></p>
+				<p><?php echo $lang['FOOTER_MORE']; ?></p>
+				
+			</section>
+			
+	<!-- Footer -->
+		<footer id="footer">
+			<?php include 'footer.inc'; ?>
+		</footer>
+
+		<script src="/js/accordion.js" type="text/javascript"></script>
+	</body>
+</html>
