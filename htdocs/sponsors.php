f6640488ef87f3f4823d99583d752d66bdf7decb
Roel Huybrechts <roel.huybrechts@geosparc.com>
2024-08-23 19:25:51 +0200

    Update sponsors.

--- htdocs/sponsors.php
+++ htdocs/sponsors.php
@@ -53,7 +53,7 @@
 							<dd class="accordionItem">
 								<a class="sponsor icon" href="https://www.opengeogroep.nl" style="background-image: url('/images/sponsors/opengeogroep.png');"></a>
 								<a class="sponsor icon" href="https://www.geocat.net/" style="background-image: url('/images/sponsors/geocat.svg');"></a>
-								<a class="sponsor icon" href="https://www.geoexperts.nl/" style="background-image: url('/images/sponsors/geoexperts.svg');"></a>
+								<a class="sponsor icon" href="https://www.geoexperts.nl/" style="background-image: url('/images/sponsors/geoexperts.png');"></a>
 
 							</dd>
 							<dt class="spons">
