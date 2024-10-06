fe310af31c2ae2708b6340ceb1240857a0fbfb0d
Roel Huybrechts <roel.huybrechts@geosparc.com>
2024-10-06 11:37:25 +0200

    Update.

--- htdocs/talk/leia-the-open-source-cesium-based-3d-viewer.php
+++ htdocs/talk/leia-the-open-source-cesium-based-3d-viewer.php
@@ -59,11 +59,11 @@ $languages = array(0 => "?", 1 => "nl", 2 => "fr", 3 => "en");
             <div class="prog-speaker">Kamiel Verhelst</div>
             <div class="prog-lang icon"><?php echo $lang['LANG_EN']; ?></div>
             <div class="prog-theme icon">3D</div>
+            <div class="prog-slides icon"><a href="/talk-slides/leia-the-open-source-cesium-based-3d-viewer.pptx"><?php echo $lang['DOWNLOAD_SLIDES'] ?></a>
             <div class="prog-tl">
                 <div class="prog-sched icon"><?php echo $lang['PROG_THU'] ?>, 13h40 - 14h00</div>
                 <div class="prog-track tr2 icon">Grote zaal</div>
             </div>
-            <div class="prog-slides"><a href="/talk-slides/leia-the-open-source-cesium-based-3d-viewer.pptx"><?php echo $lang['DOWNLOAD_SLIDES'] ?></a>
             </div>
 
             <div class="abstract">
