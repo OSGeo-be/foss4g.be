5b5d8c1efec3c9c9dc2d0cd3c43ca733fa0bbed7
Roel Huybrechts <roel.huybrechts@geosparc.com>
2024-10-06 12:38:55 +0200

    Add slides.

--- htdocs/programme.php
+++ htdocs/programme.php
@@ -805,7 +805,7 @@ $languages = array(0 => "?", 1 => "nl", 2 => "fr", 3 => "en");
 							<div class="prog-sched icon">15h25 - 15h45</div>
 							<div class="prog-track icon">Grote zaal</div>
 						</div>
-						<a class="prog-link icon" target="_blank" href="/talk/mapbox-cloud-vector-tiles-met-attribuutinformatie.php" title="<?php echo $lang['PROG_MORE_INFO']; ?>"></a>
+						<a class="prog-link prog-slides icon" target="_blank" href="/talk/mapbox-cloud-vector-tiles-met-attribuutinformatie.php" title="<?php echo $lang['PROG_MORE_INFO']; ?>"></a>
 						</div>
 						</div>
 
--- htdocs/talk/mapbox-cloud-vector-tiles-met-attribuutinformatie.php
+++ htdocs/talk/mapbox-cloud-vector-tiles-met-attribuutinformatie.php
@@ -59,6 +59,7 @@ $languages = array(0 => "?", 1 => "nl", 2 => "fr", 3 => "en");
             <div class="prog-speaker">Andrea van Milgen, Wouter van der Hulst</div>
             <div class="prog-lang icon"><?php echo $lang['LANG_EN']; ?></div>
             <div class="prog-theme icon">OSM/Mapping</div>
+            <div class="prog-slides icon"><a href="/talk-slides/mapbox-cloud-vector-tiles-met-attribuutinformatie.pptx"><?php echo $lang['DOWNLOAD_SLIDES'] ?></a>
             <div class="prog-tl">
                 <div class="prog-sched icon"><?php echo $lang['PROG_THU'] ?>, 15h25 - 15h45</div>
                 <div class="prog-track tr2 icon">Grote zaal</div>
